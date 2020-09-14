db76883bd3c19e6c5ed82b3441f1cdacac9c689a
Viper <756627124@qq.com>
2020-09-14 22:41:16 +0800

    9.14完课

---
+++ src/test/TCPServer.java
@@ -0,0 +1,15 @@
+package test;
+
+import java.net.ServerSocket;
+import java.net.Socket;
+
+/**
+ * description:
+ * author:蔡诚杰;
+ * date; 2020/9/14
+ */
+public class TCPServer {
+    private int port;
+    private ServerSocket serverSocket;
+
+}
