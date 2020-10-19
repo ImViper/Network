56190faef254b625543eb4c81f7a5c49eb680ca1
Viper <756627124@qq.com>
2020-10-19 09:47:02 +0800

    ch07

---
+++ src/chapter07/BASE64.java
@@ -0,0 +1,20 @@
+package chapter07;
+
+/**
+ * Author:蔡诚杰
+ * Data:2020/10/19
+ * description:
+ */
+public class BASE64 {
+    public static void main(String[] args) {
+        String userName="你的完整邮件名";
+        String authCode = "你的邮箱授权码";
+
+        System.out.println(encode(userName));
+        System.out.println(encode(authCode));
+    }
+
+    private static String  encode(String str) {
+        return new sun.misc.BASE64Encoder().encode(str.getBytes());
+    }
+}
